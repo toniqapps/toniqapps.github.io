181cd36f90f8fb61f983ca41a9dc21cabedbe6bb
toniqapps <toniqapps@gmail.com>
2019-09-10 11:36:22 +0530

    pelican conf updated

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,14 +3,23 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Rahul'
-SITENAME = 'Rahul'
+SITENAME = 'Toniq Apps'
 SITEURL = 'http://toniqapps.github.io'
+SITESUBTITLE = 'Full Stack Developer'
+SITEDESCRIPTION = '%s\'s Thoughts and Writings' % AUTHOR
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Asia/Kolkata'
 
 DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 THEME = "/Users/arsenal/pelican-themes/hyde"
 SITELOGO = '/images/toniq_logo.png?s=120'
@@ -22,17 +31,14 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('linkedin', 'https://linkedin.com/in/rahuldsce'),
+          ('github', 'https://github.com/toniqapps'))
 
-DEFAULT_PAGINATION = False
+COPYRIGHT_YEAR = 2018
+
+DEFAULT_PAGINATION = 10
+
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
